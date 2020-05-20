073471aafa2394176b40a7ae4142e258d0d74c18
Lucas Colla <colla.lucas@gmail.com>
2020-05-20 17:06:09 -0300

    troca de tema do JQuery Mobile para Bootstrap e atualizado calculo do INSS para tabela 2020

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SalarioAmarrador
 Página criada com o intuito de ajudar os Amarradores da Codesp a calcular o próprio salário baseado nos adicionais de horas extras, noturnas e etc...
+
+Atualizado INSS e IRRF 2020
+
+Este calculo todo é específico para uma proffisão, mas os calculos de INSS e IRRF podem ser reaproveitados...
+
+Lembrando se caso for reutilizar, o cálculo do INSS é baseado no BRUTO enquanto o IRRF é descontado o valor que ja foi retirado do INSS.
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+document.getElementById("btn_calcular").onclick = calcular;
+
 function calcular() {
     //declaração de variaveis
     var salBase = Number(document.getElementById("sal_base").value);
@@ -10,8 +12,10 @@ function calcular() {
     var diasUteis = 0;
     var inss = 0;
     var valorVT = 0;
-    var aliquota = 0;
-    var deducao = 0;
+    var aliquota_inss = 0;
+    var deducao_inss = 0;
+    var aliquota_irrf = 0;
+    var deducao_irrf = 0;
     var vt = false;
     var vtcheck = document.getElementById("vt");
 
@@ -30,72 +34,91 @@ function calcular() {
 
     var valorHoraExtra = (valorHora * 2) * horaExtra;
     var txtHoraExtra = document.getElementById("txtHoraExtra");
-    txtHoraExtra.textContent = "Hora Extra: R$ " + Number(valorHoraExtra).toFixed(2);
+    txtHoraExtra.textContent = "Hora Extra: " + Number(valorHoraExtra).toFixed(2);
 
     var valorHoraNoturna = (valorHora * 0.5) * horaNoturna;
     var txtHoraNoturna = document.getElementById("txtHoraNoturna");
-    txtHoraNoturna.textContent = "Hora Noturna: R$ " + Number(valorHoraNoturna).toFixed(2);
+    txtHoraNoturna.textContent = "Hora Noturna: " + Number(valorHoraNoturna).toFixed(2);
 
     var valorHoraRisco = (valorHora * 0.4) * horaRisco;
     var txtHoraRisco = document.getElementById("txtHoraRisco");
-    txtHoraRisco.textContent = "Adicional Risco: R$ " + Number(valorHoraRisco).toFixed(2);
+    txtHoraRisco.textContent = "Adicional Risco: " + Number(valorHoraRisco).toFixed(2);
 
     var valorDsr = (((valorHoraExtra + valorHoraNoturna + valorHoraRisco) / diasUteis) * domingos);
     var txtDSR = document.getElementById("txtDSR");
-    txtDSR.textContent = "DSR Variável: R$ " + Number(valorDsr).toFixed(2);
+    txtDSR.textContent = "DSR Variável: " + Number(valorDsr).toFixed(2);
 
     //Calculo Bruto
     var totalSoma = (salBase + valorHoraExtra + valorHoraNoturna + valorHoraRisco + valorDsr);
     var txtSomaTotal = document.getElementById("totalSoma");
-    txtSomaTotal.textContent = "Salário Bruto: R$ " + Number(totalSoma).toFixed(2);
+    txtSomaTotal.textContent = "Salário Bruto: " + Number(totalSoma).toFixed(2);
 
-    //Calculo do INSS
-    if (totalSoma < 1556.95) { inss = totalSoma * 0.08; }
-    else if (totalSoma >= 1556.95 && totalSoma < 2594.93) { inss = totalSoma * 0.09; }
-    else { inss = totalSoma * 0.11; }
+    //Calculo do INSS --- ATUALIZADO 2020 ---
+    if (totalSoma < 1045.1) {
+        aliquota_inss = 0.075;
+        deducao_inss = 0;
+    }
+    else if (totalSoma >= 1045.1 && totalSoma < 2089.61) {
+        aliquota_inss = 0.09;
+        deducao_inss = 15.684;
+    }
+    else if (totalSoma >= 2089.61 && totalSoma < 3134.41) {
+        aliquota_inss = 0.12;
+        deducao_inss = 78.378;
+    }
+    else if (totalSoma >= 3134.41 && totalSoma < 6101.07) {
+        aliquota_inss = 0.14;
+        deducao_inss = 141.068;
+    }
+    else {
+        aliquota_inss = 0;
+        deducao_inss = -713.08;
+    }
+
+    inss = (totalSoma * aliquota_inss - deducao_inss);
     var txtINSS = document.getElementById("txtINSS");
-    txtINSS.textContent = "Desconto Inss: R$ " + Number(inss).toFixed(2);
+    txtINSS.textContent = "Desconto Inss: " + Number(inss).toFixed(2);
 
 
     //Calculo do VT
     if (vt == true) { valorVT = salBase * 0.06; }
     else { valorVT = 0; }
     var txtVT = document.getElementById("txtVT");
-    txtVT.textContent = "Desconto VT: R$ " + Number(valorVT).toFixed(2);
+    txtVT.textContent = "Desconto VT: " + Number(valorVT).toFixed(2);
 
     //Calculo do VR
     var vr = salBase * 0.01;
     var txtVR = document.getElementById("txtVR");
-    txtVR.textContent = "Desconto VR: R$ " + Number(vr).toFixed(2);
+    txtVR.textContent = "Desconto VR: " + Number(vr).toFixed(2);
 
     //Calculo Imposto de Renda
     var vlrDependentes = qtdDependentes * 189.59;
     var calculoRenda = totalSoma - inss - vlrDependentes;
     if (calculoRenda < 1903.99) {
-        aliquota = 0;
-        deducao = 0;
+        aliquota_irrf = 0;
+        deducao_irrf = 0;
     }
-    else if (calculoRenda >= 1903.99 && calculoRenda < 2826.66) {
-        aliquota = 0.075;
-        deducao = 142.80;
+    else if (calculoRenda >= 1903.99 && calculoRenda < 2836.66) {
+        aliquota_irrf = 0.075;
+        deducao_irrf = 142.80;
     }
-    else if (calculoRenda >= 2826.66 && calculoRenda < 3751.06) {
-        aliquota = 0.15;
-        deducao = 354.80;
+    else if (calculoRenda >= 2836.66 && calculoRenda < 3751.06) {
+        aliquota_irrf = 0.15;
+        deducao_irrf = 354.80;
     }
     else if (calculoRenda >= 3751.06 && calculoRenda < 4664.68) {
-        aliquota = 0.225;
-        deducao = 636.13;
+        aliquota_irrf = 0.225;
+        deducao_irrf = 636.13;
     }
     else {
-        aliquota = 0.275;
-        deducao = 869.36;
+        aliquota_irrf = 0.275;
+        deducao_irrf = 869.36;
     }
 
 
-    var irrf = (calculoRenda * aliquota - deducao);
+    var irrf = (calculoRenda * aliquota_irrf - deducao_irrf);
     var txtIRRF = document.getElementById("txtIRRF");
-    txtIRRF.textContent = "Retido na fonte: R$ " + Number(irrf).toFixed(2);
+    txtIRRF.textContent = "Retido na fonte: " + Number(irrf).toFixed(2);
 
 
     //Calculo descontos
@@ -104,5 +127,5 @@ function calcular() {
     //Calculo liquido
     var salLiquido = totalSoma - totalDescontos;
     var txtLiquido = document.getElementById("salLiquido");
-    txtLiquido.textContent = "Salário Líquido: R$ " + Number(salLiquido).toFixed(2);
+    txtLiquido.textContent = "Salário Líquido: " + Number(salLiquido).toFixed(2);
 }
